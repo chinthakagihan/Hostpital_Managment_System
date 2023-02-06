ae25b9a6eaf2fd7d640af088fe91a50da7bbfd70
chinthaka gihan <122426733+chinthakagihan@users.noreply.github.com>
2023-02-06 15:15:13 +0530

    all JAVA Files

---
+++ AIP_Hospital_management_Systems.java
@@ -0,0 +1,9 @@
+
+package aip_hospital_management_systems;
+
+public class AIP_Hospital_management_Systems {
+    public static void main(String[] args) {
+        Main_menu.main(new String[0]);
+    }
+    
+}
